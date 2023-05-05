20777cddde25898822c5639b24d3d40dba4f683a
Andre Wang <awan0010@student.monash.edu>
2023-05-05 17:05:27 +1000

    Create Eureka server cluster with each server host registering each other

--- commerce-eureka-server-9002/src/main/java/com/springcloud/EurekaApplication9002.java
+++ commerce-eureka-server-9002/src/main/java/com/springcloud/EurekaApplication9002.java
@@ -2,17 +2,17 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Andre Wang
  * @version 1.0
  */
-//@EnableEurekaServer 表示该程序,作为 Eureka Server
-@EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+@EnableEurekaServer
+public class EurekaApplication9002 {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaApplication9002.class, args);
     }
 }
